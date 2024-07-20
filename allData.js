b878e82cd271fa67d28a4e191e17898d8709e192
khaled-ahmed-mohamed <143337127+khaled-ahmed-mohamed@users.noreply.github.com>
2024-07-20 15:39:50 +0300

    Add files via upload

---
+++ allData.js
@@ -0,0 +1,16 @@
+const firstName="isalm"
+const lastName="ali"
+const city="assiout"
+
+
+
+function mul1(x , y ){
+    console.log(x*y)
+  }
+
+module.exports = {
+  fname:firstName,
+  lname:lastName,
+  city:city,
+  fun1:mul1
+}
